8bbafaeb295d5d5329b1a3b489e4c0f6a45e8b60
Angellomon <angelmtzdiaz@gmail.com>
2022-02-08 19:49:10 -0600

    chore(config): demás config

--- app/config.py
+++ app/config.py
@@ -1,6 +1,15 @@
+from functools import lru_cache
+
 from pydantic import AnyUrl, BaseSettings
 
 
 class Settings(BaseSettings):
+    DB_NAME: str = "FECHAS_CDF"
     REDIS_URL: AnyUrl
     REDIS_TLS_URL: AnyUrl
+    MONGODB_URL: AnyUrl
+
+
+@lru_cache
+def get_settings():
+    return Settings(**{})
