8565e34373df2efaf0eb61e8f3eb46d9e71c961a
Angellomon <angelmtzdiaz@gmail.com>
2022-02-10 16:10:06 -0600

    feat(main): configuración de evento 'startup'

--- app/main.py
+++ app/main.py
@@ -1,16 +1,30 @@
+from beanie import init_beanie
 from fastapi import FastAPI
-from loguru import logger
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 
+from app.config import get_settings
+from app.core.default import initial_setup
+from app.db.mongodb import get_mongodb
+from app.models.productos import ProductoDB
 from app.services.redis import get_redis
+
+from .api.api import router as api_router
 
 app = FastAPI()
 
 
-@app.get("/test")
-async def test_redis():
+app.include_router(api_router)
+
+
+@app.on_event("startup")
+async def on_startup():
+    s = get_settings()
 
     r = get_redis()
+    FastAPICache.init(RedisBackend(r), prefix="fastapi-cache")
 
-    result = await r.get("test")
+    mongo_client = get_mongodb()
+    await init_beanie(mongo_client[s.DB_NAME], document_models=[ProductoDB])
 
-    logger.debug(f"result: {result}")
+    await initial_setup()
