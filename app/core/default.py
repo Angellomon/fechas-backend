8057c0d7e99dfd97602c82483b771a1b720c286f
Angellomon <angelmtzdiaz@gmail.com>
2022-02-10 16:06:56 -0600

    feat(core/defaults): definición de setup inicial

--- app/core/default.py
+++ app/core/default.py
@@ -1,7 +1,18 @@
-from app.models.productos import Producto
+from app.models.productos import Producto, ProductoDB
+
+_default_nombre = "__base__"
 
 
 def get_default_producto():
-    return Producto(
-        nombre="__base__",
+    return ProductoDB(
+        nombre=_default_nombre,
     )
+
+
+async def initial_setup():
+    producto_base = await ProductoDB.find_one(ProductoDB.nombre == _default_nombre)
+
+    if producto_base is None:
+        producto_base = get_default_producto()
+
+        await producto_base.insert()
